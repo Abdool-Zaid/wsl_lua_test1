da2b340f3db2285672f6e8188415f7127652069f
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-11 15:16:12 +0200

    added rock

--- hello_world.lua
+++ hello_world.lua
@@ -1,3 +1,9 @@
+-- header
+local version = _VERSION:match("%d+%.%d+") 
+package.path = "./dep/share/lua/" .. version .. "/?.lua;./dep/share/lua/" .. version .. "/?/init.lua;" .. package.path
+package.cpath = "./dep/lib/lua/" .. version .. "/?.so;" .. package.cpath
+
+
 local article = require 'sub_rock_test'
 local say = require 'say'
 say:set('out', article.test_str)
